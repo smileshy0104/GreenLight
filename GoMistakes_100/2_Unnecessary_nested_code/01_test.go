5d5cd7337ffe5deaf17f604c7903dbf2bb1509ff
smileshy0104 <1259581033@qq.com>
2025-01-13 21:30:53 +0800

    HttpServer相关内容

---
+++ GoMistakes_100/2_Unnecessary_nested_code/01_test.go
@@ -0,0 +1,12 @@
+package __Unnecessary_nested_code
+
+import (
+	"testing"
+)
+
+// TODO: 02、不必要的嵌套代码
+
+func Test_01(t *testing.T) {
+
+	//sort.Interface()
+}
---
+++ GreenLight/cmd/api/HttpServer_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+// 定义应用程序的版本号。
+const version = "1.0.0"
+
+// 配置结构体，用于存储服务器端口和环境变量。
+type config struct {
+	port int
+	env  string
+}
+
+// application 结构体包含配置和日志记录器。
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+// TestHttpServer 是一个测试函数，用于启动 HTTP 服务器。
+// 主要功能包括解析命令行参数、初始化日志记录器和应用程序结构体、设置路由并启动服务器。
+func TestHttpServer(t *testing.T) {
+	var cfg config
+
+	// 解析命令行参数以初始化配置。
+	// TODO 端口和环境变量都可以自定义
+	// go run ./cmd/api -port=3030 -env=production
+	flag.IntVar(&cfg.port, "port", 4000, "API 服务器端口")
+	flag.StringVar(&cfg.env, "env", "development", "环境 (development|staging|production)")
+	flag.Parse()
+
+	// 初始化日志记录器。
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// 初始化请求路由器。
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// 配置 HTTP 服务器。
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// 启动 HTTP 服务器。
+	logger.Printf("正在启动 %s 环境下的服务器，监听地址为 %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+}
+
+// healthcheckHandler 是健康检查端点的处理函数。
+// 它将服务的可用性、环境和版本信息写入响应中。
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "状态: 可用")
+	fmt.Fprintf(w, "环境: %s\n", app.config.env)
+	fmt.Fprintf(w, "版本: %s\n", version)
+}
