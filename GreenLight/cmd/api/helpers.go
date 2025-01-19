ed56a6bb1bbffcfe94ab989ff3f8dc9ba9af35fc
smileshy0104 <1259581033@qq.com>
2025-01-19 11:30:56 +0800

    解码 JSON 请求体到提供的目标中，可以使用json.Decoder/json.Unmarshal进行

--- GreenLight/cmd/api/helpers.go
+++ GreenLight/cmd/api/helpers.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"io"
 	"net/http"
 	"strconv"
 )
@@ -60,5 +62,65 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+	return nil
+}
+
+// readJSON 尝试读取并解码 JSON 请求体到提供的目标指针中。
+// 它处理各种错误情况，并返回适当的错误消息。
+// 参数：
+//   - w: http.ResponseWriter，用于写入 HTTP 响应。
+//   - r: *http.Request，包含 HTTP 请求信息。
+//   - dst: interface{}，指向要解码的目标的指针。
+//
+// 返回值：
+//   - error：如果发生错误，则返回错误；否则返回 nil。
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	// Use io.ReadAll() to read the entire request body into a []byte slice.
+	//body, err := io.ReadAll(r.Body)
+	//if err != nil {
+	//	app.serverErrorResponse(w, r, err)
+	//	return
+	//}
+	// TODO 使用Unmarshal解码请求体到目标指针中
+	//json.Unmarshal(body, dst)
+
+	// TODO 使用Decode解码请求体到目标指针中
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		// 如果解码过程中出现错误，开始分类处理
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		// 检查是否为语法错误，并返回带有错误位置的详细信息
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("请求体包含格式错误的 JSON（在字符 %d 处）", syntaxError.Offset)
+
+		// 检查是否为意外结束符错误，通常由 JSON 语法错误引起
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("请求体包含格式错误的 JSON")
+
+		// 检查是否为类型不匹配错误，并返回带有字段名称的详细信息
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("请求体包含字段 %q 类型错误", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("请求体包含类型错误（在字符 %d 处）", unmarshalTypeError.Offset)
+
+		// 检查是否为空请求体错误
+		case errors.Is(err, io.EOF):
+			return errors.New("请求体不能为空")
+
+		// 检查是否为无效解码错误，并触发 panic
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+		// 其他未知错误直接返回
+		default:
+			return err
+		}
+	}
+
 	return nil
 }
--- GreenLight/cmd/api/movies.go
+++ GreenLight/cmd/api/movies.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"DesignMode/GreenLight/internal/data"
+	"encoding/json"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"io"
 	"net/http"
 	"strconv"
 	"time"
@@ -11,9 +13,88 @@ import (
 
 // TODO 该文件存储对应的movies相关的业务函数
 
-// createMovieHandler 创建Movie
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+// createMovieHandlerOld 创建Movie
+func (app *application) createMovieHandlerOld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create a new movie")
+}
+
+// createMovieHandler 创建Movie（使用json.Decoder进行解析）
+func (app *application) createMovieHandlerOld1(w http.ResponseWriter, r *http.Request) {
+	// 声明一个匿名结构体，用于存储从 HTTP 请求体中预期获取的信息。
+	// 该结构体的字段名和类型是之前创建的 Movie 结构体的一个子集。
+	// 这个结构体将作为解码Decode的目标目标。
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// 初始化一个新的 json.Decoder 实例，该实例从请求体中读取数据，
+	// 然后使用 Decode() 方法将请求体内容解码到 input 结构体中。
+	// 注意，在调用 Decode() 时，我们传递的是 input 结构体的指针作为解码的目标。
+	// 如果解码过程中出现错误，我们将使用通用的 errorResponse() 辅助函数
+	// 向客户端发送一个 400 Bad Request 响应，并包含错误信息。
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// 将 input 结构体的内容以 HTTP 响应的形式输出。
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+// createMovieHandler 创建Movie（使用json.Unmarshal进行解析）
+func (app *application) createMovieHandlerOld2(w http.ResponseWriter, r *http.Request) {
+	// 声明一个匿名结构体，用于存储从 HTTP 请求体中预期获取的信息。
+	// 该结构体的字段名和类型是之前创建的 Movie 结构体的一个子集。
+	// 这个结构体将作为解码Decode的目标目标。
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// Use io.ReadAll() to read the entire request body into a []byte slice.
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// 初始化一个新的 json.Unmarshal 实例，该实例从请求体中读取数据，
+	// 如果解码过程中出现错误，我们将使用通用的 errorResponse() 辅助函数
+	// 向客户端发送一个 400 Bad Request 响应，并包含错误信息。
+	err = json.Unmarshal(body, &input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// 将 input 结构体的内容以 HTTP 响应的形式输出。
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+// createMovieHandler 创建Movie（使用封装函数app.readJSON(w, r, &input)）
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+	// Use the new readJSON() helper to decode the request body into the input struct.
+	// If this returns an error we send the client the error message along with a 400
+	// Bad Request status code, just like before.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		//app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // showMovieHandlerOld1 查看Movie（使用httprouter.ParamsFromContext(r.Context())读取路由）
