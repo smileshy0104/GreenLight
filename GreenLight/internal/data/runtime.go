95e82c1d65b54e6c50b5d5f6df3db9a3da848083
smileshy0104 <1259581033@qq.com>
2025-01-19 10:28:07 +0800

    自定义类型Runtime，同时是实现了自定义MarshalJSON和UnmarshalJSON

--- GreenLight/internal/data/runtime.go
+++ GreenLight/internal/data/runtime.go
@@ -7,61 +7,65 @@ import (
 	"strings"
 )
 
-// ErrInvalidRuntimeFormat returns error when we are unable to parse or convert a JSON string
-// successfully. This is used in our Runtime.UnmarshalJSON() method.
+// TODO 自定义类型Runtime，同时是实现了自定义MarshalJSON和UnmarshalJSON
+// ErrInvalidRuntimeFormat 在无法解析或转换 JSON 字符串时返回错误。
+// 该错误在 Runtime.UnmarshalJSON() 方法中使用。
 var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
+// Runtime 表示电影的时长，使用 int32 类型。
+// 自定义类型
 type Runtime int32
 
-// MarshalJSON method on the Runtime type so that it satisfies the
-// json.Marshaler interface. This should return the JSON-encoded string for the movie
-// runtime in the format "<runtime> mins".
+// MarshalJSON 实现了 json.Marshaler 接口 interface，用于将 Runtime 类型编码为 JSON。
+// 它返回一个格式为 "<runtime> mins" 的 JSON 编码字符串。
+//
+// 返回值:
+// - []byte: JSON 编码后的字节切片
+// - error: 如果发生错误则返回非空错误
+// r Runtime 使用值接收器是因为：值方法可以在指针和值上调用，但指针方法只能在指针上调用！这极大增加了对应的灵活性
 func (r Runtime) MarshalJSON() ([]byte, error) {
-	// Generate a string containing the movie runtime in the required format
+	// 生成符合要求格式的字符串
 	jsonValue := fmt.Sprintf("%d mins", r)
 
-	// Use the strconv.Quote() function on the string to wrap it double quotes.
-	// It needs to be surrounded by double quotes in order to be a valid JSON string.
+	// 使用 strconv.Quote() 函数将字符串包裹在双引号中，使其成为————有效的 JSON 字符串
+	// 否则，它将不会被解释为一个JSON字符串
 	quotedJSONValue := strconv.Quote(jsonValue)
 
-	// Convert the quoted string value to a byte slice and return it
+	// 将带引号的字符串转换为字节切片并返回
 	return []byte(quotedJSONValue), nil
 }
 
-// UnmarshalJSON ensures that Runtime satisfies the
-// json.Unmarshaler interface. IMPORTANT: because UnmarshalJSON() needs to modify the
-// receiver (our Runtime type), we must use a pointer receiver for this to work
-// correctly. Otherwise, we will only be modifying a copy (which is then discarded when
-// this method returns).
+// UnmarshalJSON 实现了 json.Unmarshaler 接口，用于将 JSON 解码为 Runtime 类型。
+// 注意：因为 UnmarshalJSON() 需要修改接收者（Runtime 类型），所以必须使用指针接收者。
+// 否则，我们只会修改一个副本（当方法返回时该副本会被丢弃）。
+//
+// 参数:
+// - jsonValue: 包含 JSON 数据的字节切片
+//
+// 返回值:
+// - error: 如果发生错误则返回非空错误
 func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
-	// We expect that the incoming JSON value will be a string in the format
-	// "<runtime> mins", and the first thing we need to do is remove the surrounding
-	// double-quotes from this string. If we can't unquote it, then we return the
-	// ErrInvalidRuntimeFormat error.
+	// 去除字符串周围的双引号。如果无法取消引用，则返回 ErrInvalidRuntimeFormat 错误
 	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
 	if err != nil {
 		return ErrInvalidRuntimeFormat
 	}
 
-	// Split the string to isolate the part containing the number.
+	// 拆分字符串以隔离数字部分
 	parts := strings.Split(unquotedJSONValue, " ")
 
-	// Sanity check the parts of the string to make sure it was in the expected format.
-	// If it wasn't, we return the ErrInvalidRuntimeFormat error again.
+	// 检查字符串是否符合预期格式。如果不符，则返回 ErrInvalidRuntimeFormat 错误
 	if len(parts) != 2 || parts[1] != "mins" {
 		return ErrInvalidRuntimeFormat
 	}
 
-	// Otherwise, parse the string containing the number into an int32. Again, if this
-	// fails return the ErrInvalidRuntimeFormat error.
+	// 将包含数字的字符串解析为 int32。如果解析失败，则返回 ErrInvalidRuntimeFormat 错误
 	i, err := strconv.ParseInt(parts[0], 10, 32)
 	if err != nil {
 		return ErrInvalidRuntimeFormat
 	}
 
-	// Convert the int32 to a Runtime type and assign this to the receiver. Note that we
-	// use the * operator to deference the receiver (which is a pointer to a Runtime
-	// type) in order to set the underlying value of the pointer.
+	// 将 int32 转换为 Runtime 类型并赋值给接收者
 	*r = Runtime(i)
 
 	return nil
